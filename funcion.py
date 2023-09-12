1ec3e78778d36e18296ce96cc665035430b652f6
309 <309@academicos.cesde.edu.co>
2023-09-12 06:42:53 -0500

    feat: se crea funcion saludar persona

---
+++ funcion.py
@@ -0,0 +1,5 @@
+#funcion que recibe el nombre de una persona y la saluda
+#DECLARAR
+def saludarPersona(nombre):
+    print(f"Hols buenos dias dormilon {nombre}")
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from funcion1 import saludarPersona
+
+#Usar una funcion=llamarla
+saludarPersona("Evelin Moreno")
